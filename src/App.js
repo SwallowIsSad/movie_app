35effc88de133ecdcba742ea24cf9de747d81e11
jang <97jjy97@naver.com>
2019-12-01 23:21:15 +0900

    리스트 불러오는 로직 작성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import axios from 'axios';
+import Movie from './Movie';
 
-function App() {
-  return (
-    <div className="App">
-        hi
+class App extends React.Component{
+  state = {
+    isLoading : true,
+    movies : []
+  };
+  getMovies = async ()=> {
+    const {data : {data : {movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    this.setState({movies, isLoading : false});
+  };
+  componentDidMount(){
+    this.getMovies();
+  }
+  render() {
+    const {isLoading, movies} = this.state;
+    return <div>
+      {isLoading ? "loading" : movies.map(movie => {
+      return <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.poster} />
+    })}
     </div>
-  );
+  }
 }
 
 export default App;
