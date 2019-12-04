aa9de972ba0047a29b247f0b629c35d869a0aff7
jang <97jjy97@naver.com>
2019-12-04 23:06:43 +0900

    설정 변경

--- src/App.js
+++ src/App.js
@@ -1,45 +1,45 @@
-import React from 'react';
-import axios from 'axios';
-import Movie from './Movie';
+import React from 'react'
+import axios from 'axios'
+import Movie from './Movie'
 
 class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: [],
-  };
+    state = {
+        isLoading: true,
+        movies: []
+    }
 
-  getMovies = async () => {
-    const {
-      data: {
-        data: { movies },
-      },
-    } = await axios.get('https://yts-proxy.now.sh/list_movies.json');
-    this.setState({ movies, isLoading: false });
-  };
+    getMovies = async () => {
+        const {
+            data: {
+                data: { movies }
+            }
+        } = await axios.get('https://yts-proxy.now.sh/list_movies.json')
+        this.setState({ movies, isLoading: false })
+    }
 
-  componentDidMount() {
-    this.getMovies();
-  }
+    componentDidMount() {
+        this.getMovies()
+    }
 
-  render() {
-    const { isLoading, movies } = this.state;
-    return (
-      <div>
-        {isLoading
-          ? 'loading'
-          : movies.map((movie) => (
-            <Movie
-              key={movie.id}
-              id={movie.id}
-              year={movie.year}
-              title={movie.title}
-              summary={movie.summary}
-              poster={movie.medium_cover_image}
-            />
-          ))}
-      </div>
-    );
-  }
+    render() {
+        const { isLoading, movies } = this.state
+        return (
+            <section class="container">
+                {isLoading
+                    ? 'loading'
+                    : movies.map(movie => (
+                          <Movie
+                              key={movie.id}
+                              id={movie.id}
+                              year={movie.year}
+                              title={movie.title}
+                              summary={movie.summary}
+                              poster={movie.medium_cover_image}
+                          />
+                      ))}
+            </section>
+        )
+    }
 }
 
-export default App;
+export default App
