c36852a932f4e07873af2f828aa165e0fa2ea1b6
jang <97jjy97@naver.com>
2019-12-05 23:21:33 +0900

    데이터 불러오는 요소 추가

--- src/Movie.js
+++ src/Movie.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 function Movie({ id, year, title, summary, poster }) {
-  return <div>{title}</div>;
+  return (
+    <div className="movie">
+      <h2 className="movie_title">{title}</h2>
+      <h2 className="movie_year">{year}</h2>
+      <h2 className="movie_summary">{summary}</h2>
+    </div>
+  );
 }
 
 export default Movie;
